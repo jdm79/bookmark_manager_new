fda0595c8f4a0ccc88cdcdcb0428410f1d8cec3c
jdm79 <james.malvern@gmail.com>
2019-02-09 23:05:29 +0000

    test: adds test to check if specific urls are present on the bookmarks route

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
 
 class BookmarkManager < Sinatra::Base
-  get '/' do
-    'Bookmark Manager'
+  get '/bookmarks' do
+    bookmarks = [
+      "http://www.makersacademy.com",
+      "http://www.destroyallsoftware.com",
+      "http://www.google.com"
+     ]
+
+     bookmarks.join
   end
 
   run! if app_file == $0
